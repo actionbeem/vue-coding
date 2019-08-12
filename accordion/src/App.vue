754b27d63347bbbe609db7e08910ce373a734f17
김종혁 <actionbeem929@gmail.com>
2019-08-12 18:07:08 +0900

    accordion ui 생성

---
+++ accordion/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <accordion></accordion>
+  </div>
+</template>
+
+<script>
+import Accordion from './components/Accordion.vue'
+
+export default {
+  components: {
+    Accordion
+  }
+}
+</script>
+
+<style>
+body { background-color:#f6f6f6; }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul,li,p { padding:0; margin:0; }
+</style>
---
+++ accordion/src/components/Accordion.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <ul class="accordion-menu">
+      <li class="tab" v-for="menu in sideMenu" :key="menu">
+        <p class="tit">{{ menu.title }}</p>
+        <ul class="dep2">
+          <li v-for="list in menu.dep2" :key="list">{{ list }}</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Accordion',
+  data(){
+    return {
+      sideMenu: [
+        { 
+          title: "tab01",
+          dep2: ["aaa","bbb","ccc"]
+        },
+        { 
+          title: "tab02",
+          dep2: ["ddd","eee","fff"]
+        },
+        { 
+          title: "tab03",
+          dep2: ["zzz","xxx","yyy"]
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 { margin: 40px 0 0; }
+ul { list-style-type: none; padding: 0; }
+li { margin:0; }
+a { color: #42b983; }
+
+.wrapper { text-align:left; }
+.accordion-menu { display:inline-block; background-color:#fff; }
+.accordion-menu .tab { width:200px; cursor:pointer; }
+.accordion-menu .tab .tit { line-height:50px; text-align:left; padding:0 15px; background-color:#eee;  }
+.accordion-menu .tab .dep2 { display:none; }
+.accordion-menu .tab .dep2 li { padding:0 15px; }
+</style>
