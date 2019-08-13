be5c834286d58113e949b4c68667adc7358e8c9a
김종혁 <actionbeem929@gmail.com>
2019-08-13 18:45:44 +0900

    기능 추가

--- accordion/src/components/Accordion.vue
+++ accordion/src/components/Accordion.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <ul class="accordion-menu">
-      <li class="tab" v-for="menu in sideMenu" :key="menu">
+      <li class="tab" v-for="(menu, index) in sideMenu" :key="index" @click="toggle(index)" :class="{ active: menu.active }">
         <p class="tit">{{ menu.title }}</p>
         <ul class="dep2">
           <li v-for="list in menu.dep2" :key="list">{{ list }}</li>
@@ -19,19 +19,29 @@ export default {
       sideMenu: [
         { 
           title: "tab01",
+          active: false,
           dep2: ["aaa","bbb","ccc"]
         },
         { 
           title: "tab02",
+          active: false,
           dep2: ["ddd","eee","fff"]
         },
         { 
           title: "tab03",
+          active: false,
           dep2: ["zzz","xxx","yyy"]
         },
       ]
     }
-  }
+  },
+  methods: {
+    toggle(index){
+      this.sideMenu.forEach(val => val.active = false);
+      this.sideMenu[index].active = !this.sideMenu[index].active
+
+    }
+  },
 }
 </script>
 
@@ -43,9 +53,11 @@ li { margin:0; }
 a { color: #42b983; }
 
 .wrapper { text-align:left; }
-.accordion-menu { display:inline-block; background-color:#fff; }
+.accordion-menu { display:inline-block; background-color:#fff; padding:10px; background-color:#ccc; }
 .accordion-menu .tab { width:200px; cursor:pointer; }
 .accordion-menu .tab .tit { line-height:50px; text-align:left; padding:0 15px; background-color:#eee;  }
-.accordion-menu .tab .dep2 { display:none; }
+.accordion-menu .tab .dep2 { display:none;  }
 .accordion-menu .tab .dep2 li { padding:0 15px; }
+.accordion-menu .tab.active .dep2 { display:block; }
+
 </style>
