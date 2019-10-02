f6796abefbabd0a2c3151a64f72326101c8fe72a
김종혁 <actionbeem929@gmail.com>
2019-10-02 18:19:37 +0900

    불필요한 코드 삭제 및 파비콘 추가

--- blog_with_firebase/src/routes/index.js
+++ blog_with_firebase/src/routes/index.js
@@ -6,7 +6,6 @@ Vue.use(Router)
 import IndexPage from '../views/IndexPage.vue'
 import WritePage from '../views/WritePage.vue'
 import DetailPage from '../views/DetailPage.vue'
-import TestPage from '../views/TestPage.vue'
 
 export default new Router ({
   mode: 'history',
@@ -37,11 +36,6 @@ export default new Router ({
       path: '/post/:postId',
       name: 'detail',
       component: DetailPage
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: TestPage
     },
   ],
 })
